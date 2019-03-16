525ba290a8e143b1db0d2677ed74f34061947c36
Raj <rajpanchal377@gmail.com>
2019-03-16 16:50:42 +0530

    added cli-host.php file

---
+++ cli-host.php
@@ -0,0 +1,6 @@
+ssh username@hostname/ipname  <== For login ssh
+
+scp -r username@hostname/ipname:/host directory/ /local directory/   <== copy from host to local machine
+
+mkdir  <== For making directory
+rmdir  <== For remove directory
